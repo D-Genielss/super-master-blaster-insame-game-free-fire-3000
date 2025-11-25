62bc05385fadccd72e86df288e65154b926843ca
lucianoschmerega-tech <lucianoschmerega@alunos.utfpr.edu.br>
2025-11-25 09:37:27 -0300

    add

---
+++ super-master-blaster-insame-game-free-fire-3000-main/include/teste.h
@@ -0,0 +1,9 @@
+#ifndef TESTE_H
+#define TESTE_H
+#include <time.h>
+
+int Aleatorio(int linhas);
+int ContaLinhas(FILE *arquivo);
+void Juntar(FILE *arquivo, char palavra[][30], char dica[][30]);
+
+#endif
